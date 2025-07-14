f404bd762ad1642e8e670991a2697b7a9ebf6822
Thomas Stefen Mardianto <96872292+T0MYAMMM@users.noreply.github.com>
2025-07-14 09:53:53 +0700

    Update settings.py
    hide credentials

--- config/settings.py
+++ config/settings.py
@@ -20,20 +20,20 @@ PATH_CRAWLER_METRICS = '/mnt/data/metrics'
 PATH_CRAWLER_DAILY_DATA = '/data' # project_dir / data
 
 # KAFKA
-BOOTSTRAP_SERVER = '139.59.241.75:9092'
-VPS_SERVER = '195.35.22.151'
-PRODUCTION_SERVER_1 = '10.130.137.101:9092'
-PRODUCTION_SERVER_2 = '10.130.137.99:9092'
-PRODUCTION_SERVER_3 = '10.130.137.100:9092'
+BOOTSTRAP_SERVER = ''
+VPS_SERVER = ''
+PRODUCTION_SERVER_1 = ''
+PRODUCTION_SERVER_2 = ''
+PRODUCTION_SERVER_3 = ''
 
 # API
-CREATE_ARTICLE_ENDPOINT = 'https://v5.external.dashboard.nolimit.id/online-media/raw-uri/create'
-SEND_POST_ARTICLE_ENDPOINT = 'https://external.backend.dashboard.nolimit.id/online-media/raw-article/create' 
-API_ZYTE = '0c671085bb1743bf94aca63032c28ceb'
+CREATE_ARTICLE_ENDPOINT = ''
+SEND_POST_ARTICLE_ENDPOINT = '' 
+API_ZYTE = ''
 
 # PROXY
-LOCAL_PROXY = 'http://bandung:456xyz@proxycrawler.dashboard.nolimit.id:2570'
-ZYTE_PROXY = 'http://0c671085bb1743bf94aca63032c28ceb:@api.zyte.com:8011'
+LOCAL_PROXY = ''
+ZYTE_PROXY = ''
 
 # HEADLESS BROWSER
-PLAYWRIGHT_BROWSER_API = 'http://68.183.238.4:8000/render'
+PLAYWRIGHT_BROWSER_API = ''
